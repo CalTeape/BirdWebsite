ebdb57c11b036160c6960ff3c9b3a92692e0aec7
Callum Teape <teaca584@student.otago.ac.nz>
2022-08-11 13:46:47 +1200

    added first attempt for search filter

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
+const dataArray;
+
 async function main() {
   // const fs = require('fs');
   const response = await fetch('./data/nzbird.json');
   const d = await response.text();
   const data = JSON.parse(d);
 
+  dataArray = newArray[data.length];
   for(i = 0; i < data.length; i++){
       const birdSpecies = data[i]
       const e = document.createElement('div');
       const html = `
-        <div class="bird-tile">
+        <div class="bird-tile" id ="${birdSpecies.primary_name}">
           <div class="photo-box">
               <div class="gradient"></div>
               <img src=" ${birdSpecies.photo.source}" alt="Description of the image" />
@@ -32,12 +35,36 @@ async function main() {
         </div>
       `;
 
+      dataArray[i] = html;
       e.innerHTML = html;
       document.querySelector('.bird-subgrid').append(e);
   }
-
-
 }
+
+
+
+function searchFilter() {
+  var input, filter, maoriName, txtValue;
+  input = document.getElementById('searchFilter');
+  filter = input.value.toUpperCase();
+
+  for(int  = 0; i < dataArray.length; i++){
+    maoriName = dataArray[i].maori_name;
+    txtValue = maoriName.toUpperCase();
+
+    if(txtValue.indexOf(filter) > -1) {
+    document.getElementById(maoriName).style.display = "";
+    }
+    else{
+      document.getElementById(maoriName).style.display = "none";
+
+    }
+
+
+    
+  }
+}
+
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
